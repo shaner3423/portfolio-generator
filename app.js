4dcb8412e4307a1f06042962d3c5561bbbb2e082
Sugar Shane <scrisostomo17@gmail.com>
2021-07-15 00:28:42 -0400

    added single line strings

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const profileDataArgs = process.argv.slice(2,process.argv.length);
 // console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    //!This...
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);    
-    }
+// const printProfileData = profileDataArr => {
+//     //!This...
+//     for (let i = 0; i < profileDataArr.length; i+= 1) {
+//         console.log(profileDataArr[i]);    
+//     }
 
-    console.log('===========');
+//     console.log('===========');
 
-    //!Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem))
+//     //!Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem))
+// };
 
-};
+// printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+// const generatePage = () => 'Name: Jane, Github: janehub';
+// console.log(generatePage());
+
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(generatePage('Jane', 'janehub'));
 
 
+ 

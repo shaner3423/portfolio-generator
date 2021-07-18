b03066d39594b069196278826807205597c44497
Sugar Shane <scrisostomo17@gmail.com>
2021-07-18 00:52:56 -0400

    completed file

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
-// console.log(profileDataArgs);
+const fs = require('fs');
 
-// const printProfileData = profileDataArr => {
-//     //!This...
-//     for (let i = 0; i < profileDataArr.length; i+= 1) {
-//         console.log(profileDataArr[i]);    
-//     }
+const generatePage = require('./src/page-template.js');
 
-//     console.log('===========');
+const profileDataArgs = process.argv.slice(2);
 
-//     //!Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem))
-// };
+const [name, github] = profileDataArgs;
 
-// printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
 
-// const generatePage = () => 'Name: Jane, Github: janehub';
-// console.log(generatePage());
+    console.log ('Poftfolio complete! Check out index.html to see the output!');
+});
 
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-console.log(generatePage('Jane', 'janehub'));
+
+
+
 
 
  
